776ed25b906f9e76c245535134476c3931de286f
Julian <julian.irwin@gmail.com>
2019-10-17 09:17:29 -0500

    added triwave

--- instrpyvisa/agilent_33220a.py
+++ instrpyvisa/agilent_33220a.py
@@ -5,49 +5,50 @@ Created on Fri Oct  4 15:38:39 2019
 @author: rzchlab
 """
 
+
 class FuncGenAgilent33220(object):
     def __init__(self, address, resource_manager, verbose=False):
         idstr = f'GPIB::{address}::INSTR'
         self.instr = resource_manager.get_instrument(idstr)
         self.verbose = verbose
-        
+
     def outp(self, state):
         """Turn output on (state=1) or off (state=0)."""
         state_str = 'OFF' if state == 0 else 'ON'
         self.write(f'OUTP {state_str};')
-        
+
     def outp_on(self):
         """Turn output on (state=1) or off (state=0)."""
         self.outp(1)
-        
+
     def outp_off(self):
         """Turn output on (state=1) or off (state=0)."""
         self.outp(0)
-        
+
     def setup_sin(self, freq, vpp, offset):
         """
         Setup a sin wave output
-        
+
         Args:
             freq: frequency in hz
             vpp: peak to peak voltage
             offset: offset in volts
         """
         cmd = (f"FUNC SIN; FREQ {freq:.3f}; VOLT {vpp:.3f};"
-                f" VOLT:OFFS {offset:.3f};")
-        print(cmd)
+               f" VOLT:OFFS {offset:.3f};")
         self.instr.write(cmd)
-        
+
     def offset(self, v_offset):
         """Change offset votage"""
         cmd = f" VOLT:OFFS {v_offset:.3f};"
         self.write(cmd)
-        
+
     def write(self, cmd):
         self.instr.write(cmd)
         if self.verbose:
             print(cmd)
-        
+
+
 if __name__ == '__main__':
     import visa as vs
     rm = vs.ResourceManager()
